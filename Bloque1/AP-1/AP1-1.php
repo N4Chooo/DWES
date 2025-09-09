9ba649dff0c2b2e89b02108a9864a70a07e50084
Nacho Fayos <nafama@alumnatflorida.es>
2025-09-09 16:53:20 +0200

    AP1-1.php

--- Bloque1/AP-1/AP1-1.php
+++ Bloque1/AP-1/AP1-1.php
@@ -1,2 +1,9 @@
-<?php
-echo "prueba";
+<?
+$array = ['hola' => $_GET['hola'],
+         'clave' => $_GET['clave'],
+         'clave2' => $_GET['clave2'],];
+
+foreach ($array as $clave => $valor) {
+     echo "Se ha recibido ". $valor . " para la clave " . $clave . "<br>" ;
+}
+
