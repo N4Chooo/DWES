abe1579d51729981fc7f22d9afd3b7c9f4dca518
Nacho Fayos <nafama@alumnatflorida.es>
2025-09-09 17:21:14 +0200

    AP1-2.php

---
+++ Bloque1/AP-1/AP1-2.php
@@ -0,0 +1,15 @@
+<?php
+$array = ['hola' => $_GET['hola'],
+    'clave' => $_GET['clave'],
+    'clave2' => $_GET['clave2'],
+    'dato' => $_GET['dato']];
+
+foreach ($array as $clave => $valor) {
+    if (is_numeric($valor)){
+        echo"Se ha recibido un numero <br>";
+    }elseif (is_null($valor) || empty($valor) || $valor == "null"){
+        "No se ha recibido ningun dato o es nulo <br>";
+    }elseif (!is_numeric($valor)){
+        echo "Se ha recibido una cadena de caracteres <br>";
+    }
+}
